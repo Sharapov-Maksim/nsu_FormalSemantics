ecaf471e7331ceb9c64c498d0f362a4ad4e043f0
Sharapov-Maksim <55018975+Sharapov-Maksim@users.noreply.github.com>
2024-10-23 00:38:34 +0700

    setup ANTLR for new grammar

--- antlr_parser/antlr_parser/src/main/java/nsu/mmf/formalsemantics/Main.java
+++ antlr_parser/antlr_parser/src/main/java/nsu/mmf/formalsemantics/Main.java
@@ -1,7 +1,7 @@
 package nsu.mmf.formalsemantics;
 
-import nsu.mmf.formalsemantics.antlr.PythonLexer;
-import nsu.mmf.formalsemantics.antlr.PythonParser;
+import nsu.mmf.formalsemantics.antlr.Python3Lexer;
+import nsu.mmf.formalsemantics.antlr.Python3Parser;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
@@ -10,10 +10,10 @@ import org.antlr.v4.runtime.tree.ParseTreeWalker;
 public class Main {
 
     public static void main(String[] args) {
-        PythonLexer lexer = new PythonLexer(CharStreams.fromString("hello world"));
+        Python3Lexer lexer = new Python3Lexer(CharStreams.fromString("a = 23\n"));
         CommonTokenStream tokens = new CommonTokenStream(lexer);
-        PythonParser parser = new PythonParser(tokens);
-        ParseTree tree = parser.r();
+        Python3Parser parser = new Python3Parser(tokens);
+        ParseTree tree = parser.single_input();
         ParseTreeWalker walker = new ParseTreeWalker();
         walker.walk(new PythonWalker(), tree);
     }
--- antlr_parser/antlr_parser/src/main/java/nsu/mmf/formalsemantics/PythonWalker.java
+++ antlr_parser/antlr_parser/src/main/java/nsu/mmf/formalsemantics/PythonWalker.java
@@ -1,16 +1,22 @@
 package nsu.mmf.formalsemantics;
 
-import nsu.mmf.formalsemantics.antlr.PythonBaseListener;
-import nsu.mmf.formalsemantics.antlr.PythonParser;
+import nsu.mmf.formalsemantics.antlr.Python3Parser.Single_inputContext;
+import nsu.mmf.formalsemantics.antlr.Python3ParserBaseListener;
+import nsu.mmf.formalsemantics.antlr.Python3Parser;
 
-public class PythonWalker extends PythonBaseListener {
+public class PythonWalker extends Python3ParserBaseListener {
 
-    public void enterR(PythonParser.RContext ctx ) {
-        System.out.println( "Entering R : " + ctx.ID().getText() );
+    @Override
+    public void enterSingle_input(Single_inputContext ctx) {
+        System.out.println("Entering single_input: " + ctx.simple_stmts().getText());
     }
 
-    public void exitR(PythonParser.RContext ctx ) {
-        System.out.println( "Exiting R" );
-    }
+//    public void enterR(Python3Parser.RContext ctx ) {
+//        System.out.println( "Entering R : " + ctx.ID().getText() );
+//    }
+//
+//    public void exitR(PythonParser.RContext ctx ) {
+//        System.out.println( "Exiting R" );
+//    }
 
 }
